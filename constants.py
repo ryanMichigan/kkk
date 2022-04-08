9c3b3e93e60ce1927d5e827cdb806dfba8c7f0cf
ryanMichigan <90494935+ryanMichigan@users.noreply.github.com>
2022-04-08 22:29:14 +0800

    789

--- constants.py
+++ constants.py
@@ -20,13 +20,7 @@
 
 from pygame.locals import *
 
-# Configuration of building shape block
-# Width of the shape block
-BWIDTH     = int(height['pure'] / 20)
-# Height of the shape block
-BHEIGHT    = BWIDTH
-# Width of the line around the block
-MESH_WIDTH = 2
+import screeninfo
 
 # Configuration of the player board
 # Board line height
@@ -36,14 +30,26 @@ BOARD_UP_MARGIN  = 40
 # Margins around all lines
 BOARD_MARGIN     = 2
 
+height = {}
+height['screen'] = screeninfo.get_monitors()[0].height
+height['pure'] = height['screen'] - BOARD_HEIGHT*2 - BOARD_UP_MARGIN - BOARD_UP_MARGIN*2
+
+# Configuration of building shape block
+# Height of the shape block
+BHEIGHT    = int(height['pure'] / 20)
+# Width of the shape block
+BWIDTH     = BHEIGHT
+# Width of the line around the block
+MESH_WIDTH = 2
+
 # Color declarations in the RGB notation
 WHITE    = (255,255,255)
 RED      = (255,0,0)
 GREEN    = (0,255,0)
 BLUE     = (0,0,255)
-ORANGE   = (50,150,6)
-GOLD     = (255,125,0)
-PURPLE   = (168,24,28)
+ORANGE   = (255,69,0)
+GOLD     = (195,131,65)
+PURPLE   = (128,0,128)
 CYAN     = (0,255,255) 
 BLACK    = (0,0,0)
 
