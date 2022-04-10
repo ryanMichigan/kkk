f140c0a9083858af481616c4d512f78c6bffd63b
ryanMichigan <90494935+ryanMichigan@users.noreply.github.com>
2022-04-10 08:10:52 +0800

    43

--- triangle.c
+++ triangle.c
@@ -4,11 +4,11 @@ main () {
     scanf("%d", &n);
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n-i; j++) {
-          if (i % 2) {
-            printf("*");
-          } else {
-            printf("+");
-          }
+            if (i % 2) {
+                printf("*");
+            } else {
+                printf("+");
+            }
         }
         printf("\n");
-}
+    }
